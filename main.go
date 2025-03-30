f58bdae894623632a999b9b0b6afe0b631d57322
Adel Harrat <a03l.web@gmail.com>
2025-03-30 18:49:28 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"go/my-web-application/views"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var homeView *views.View
+var contactView *views.View
+
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if err := homeView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if err := contactView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Page not found</h1><p>The page you're looking for does not exist!</p>")
+}
+
+func main() {
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
+
+	r := httprouter.New()
+	r.GET("/", home)
+	r.GET("/contact", contact)
+	r.NotFound = http.HandlerFunc(notFound)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
